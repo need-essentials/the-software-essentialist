dc84251fd694add26b346f14dd0fa82832051167
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-03-17 09:32:33 +0100

    RED: optimistic two pointers approach

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.2 - Palindrome/src/index.ts
@@ -1,3 +1,12 @@
 export function isPalindrome(str: string): boolean {
-  throw new Error("not implemented");
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
 }
