7afeba6f61e76622f2367641a504c8e60f3b54e2
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-15 20:08:57 +0200

    🔴: Setup initial failing test for multiples of 3 and 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -10,4 +10,9 @@ describe("fizzbuzz", () => {
     expect(fizzbuzz(5)).toBe("Buzz");
     expect(fizzbuzz(10)).toBe("Buzz");
   });
+
+  test("returns FizzBuzz for multiples of 3 and 5", () => {
+    expect(fizzbuzz(15)).toBe("FizzBuzz");
+    expect(fizzbuzz(30)).toBe("FizzBuzz");
+  });
 });
