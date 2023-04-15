6c112af58b1e8c58dee41cfc33c9f1bcabd51677
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-15 20:20:52 +0200

    🟢: Implement solution for isMultipleOf function

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,4 @@
-export const isMultipleOf = (n: number, m: number) => {
-  throw new Error("Not implemented");
-};
+export const isMultipleOf = (n: number, factor: number) => n % factor === 0;
 
 export function fizzbuzz(n: number): string {
   if (n < 1 || n > 100) throw new Error("Out of range");
