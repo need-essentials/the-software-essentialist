b6b1e74156fad39ef9caa7ecee026a76f5a1e3aa
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-15 20:13:33 +0200

    🟢: Setup passing test for out of range numbers

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,6 @@
 export function fizzbuzz(n: number): string {
+  if (n < 1 || n > 100) throw new Error("Out of range");
+
   if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
   if (n % 3 === 0) return "Fizz";
   if (n % 5 === 0) return "Buzz";
