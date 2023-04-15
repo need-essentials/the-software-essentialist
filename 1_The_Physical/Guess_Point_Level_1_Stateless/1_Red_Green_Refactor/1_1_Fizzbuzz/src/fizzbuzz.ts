ac6d5bd76f4bcd20a7a5feccdffe475c86f073e7
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-15 20:24:02 +0200

    🔵: Refactor fizzbuzz to use isMultipleOf to improve readability

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,9 +3,9 @@ export const isMultipleOf = (n: number, factor: number) => n % factor === 0;
 export function fizzbuzz(n: number): string {
   if (n < 1 || n > 100) throw new Error("Out of range");
 
-  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
-  if (n % 3 === 0) return "Fizz";
-  if (n % 5 === 0) return "Buzz";
+  if (isMultipleOf(n, 3) && isMultipleOf(n, 5)) return "FizzBuzz";
+  if (isMultipleOf(n, 3)) return "Fizz";
+  if (isMultipleOf(n, 5)) return "Buzz";
 
   return n.toString();
 }
