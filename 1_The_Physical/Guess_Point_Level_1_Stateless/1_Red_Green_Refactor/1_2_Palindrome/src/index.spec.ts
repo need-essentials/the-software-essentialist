9ea37e4aa69ba02af787b30997ea7e4dc1d0782e
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-16 00:40:20 +0200

    🔴: Add tests for palindromes and non-palindromes

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -5,6 +5,18 @@ describe("palindrome checker", () => {
     expect(isPalindrome("")).toBeTruthy();
   });
 
+  test("should return true for palindrome", () => {
+    expect(isPalindrome("a")).toBeTruthy();
+    expect(isPalindrome("Mom")).toBeTruthy();
+    expect(isPalindrome("Was It A Rat I Saw")).toBeTruthy();
+    expect(isPalindrome("Never Odd or Even")).toBeTruthy();
+  });
+
+  test("should return false for non-palindrome", () => {
+    expect(isPalindrome("bill")).toBeFalsy();
+    expect(isPalindrome("hello")).toBeFalsy();
+  });
+
   describe("normalize", () => {
     test("should remove spaces", () => {
       expect(normalize("a b")).toBe("ab");
