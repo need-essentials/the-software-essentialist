c3de7d787f4c6075a3175213aedd9ac11a8e3018
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-16 00:34:22 +0200

    🔴: Add tests for case and non-alphanumeric character

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,7 +1,21 @@
-import { isPalindrome } from "./index";
+import { isPalindrome, normalize } from "./index";
 
 describe("palindrome checker", () => {
   test("should return true for empty string", () => {
     expect(isPalindrome("")).toBeTruthy();
+  });
+
+  describe("normalize", () => {
+    test("should remove spaces", () => {
+      expect(normalize("a b")).toBe("ab");
+    });
+
+    test("should remove punctuation", () => {
+      expect(normalize("a, b!")).toBe("ab");
+    });
+
+    test("should lowercase", () => {
+      expect(normalize("A")).toBe("a");
+    });
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,3 +1,7 @@
+export function normalize(str: string): string {
+  throw new Error("Not implemented");
+}
+
 export function isPalindrome(str: string): boolean {
   if (str.length === 0) {
     return true;
