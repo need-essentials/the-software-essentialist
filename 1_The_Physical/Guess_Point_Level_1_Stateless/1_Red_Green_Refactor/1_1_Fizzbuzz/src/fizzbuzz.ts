c1171a1e705f7c404a641aacfaecb354901868a2
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-15 20:30:51 +0200

    🔵: Refactor fizzbuzz to use isInRange to improve readability

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -4,7 +4,7 @@ export const isInRange = (n: number, min = 1, max = 100) =>
 export const isMultipleOf = (n: number, factor: number) => n % factor === 0;
 
 export function fizzbuzz(n: number): string {
-  if (n < 1 || n > 100) throw new Error("Out of range");
+  if (!isInRange(n)) throw new Error("Out of range");
 
   if (isMultipleOf(n, 3) && isMultipleOf(n, 5)) return "FizzBuzz";
   if (isMultipleOf(n, 3)) return "Fizz";
