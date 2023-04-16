a6da3f3dbac0c01302178f43ff81459c15ac1c69
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-16 23:07:33 +0200

    🟢: Add hour validation with isHourInRange function

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { isInRange } from "./index";
+import { isHourInRange, isInRange } from "./index";
 
 describe("military time validator", () => {
   describe("isInRange", () => {
@@ -8,6 +8,16 @@ describe("military time validator", () => {
 
     it("should return false if value is not in range", () => {
       expect(isInRange(5, 6, 10)).toBe(false);
+    });
+  });
+
+  describe("isHourInRange", () => {
+    it("should return true if hour is in range", () => {
+      expect(isHourInRange(5)).toBe(true);
+    });
+
+    it("should return false if hour is not in range", () => {
+      expect(isHourInRange(25)).toBe(false);
     });
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,2 +1,4 @@
 export const isInRange = (value: number, min: number, max: number): boolean =>
   value >= min && value <= max;
+
+export const isHourInRange = (hour: number): boolean => isInRange(hour, 0, 23);
