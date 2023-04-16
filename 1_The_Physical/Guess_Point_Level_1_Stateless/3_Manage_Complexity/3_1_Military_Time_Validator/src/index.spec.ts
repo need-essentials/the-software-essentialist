b383d882e6e53b66785a3b34b40e6b83ce95c700
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-16 23:05:31 +0200

    🟢: Implement isInRange validation function for general use

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,13 @@
+import { isInRange } from "./index";
 
-describe('military time validator', () => {
+describe("military time validator", () => {
+  describe("isInRange", () => {
+    it("should return true if value is in range", () => {
+      expect(isInRange(5, 0, 10)).toBe(true);
+    });
 
-
-})
+    it("should return false if value is not in range", () => {
+      expect(isInRange(5, 6, 10)).toBe(false);
+    });
+  });
+});
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -0,0 +1,2 @@
+export const isInRange = (value: number, min: number, max: number): boolean =>
+  value >= min && value <= max;
