98f8fa49e5fdc118f1c020ae5d92662b9cca7587
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-19 11:30:15 +0200

    🟢: Implement single value parsing

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,19 @@
+import { parseBooleanValue } from "./index";
 
-describe('boolean calculator', () => {
+describe("boolean calculator", () => {
+  describe("parseBooleanValue", () => {
+    it("should return true for 'true'", () => {
+      expect(parseBooleanValue("true")).toBeTruthy();
+    });
 
-})
+    it("should return false for 'false'", () => {
+      expect(parseBooleanValue("false")).toBeFalsy();
+    });
+
+    it("should throw an error for 'foo'", () => {
+      expect(() => parseBooleanValue("foo")).toThrowError(
+        "Invalid boolean value"
+      );
+    });
+  });
+});
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -0,0 +1,15 @@
+const BOOLEAN_VALUE = {
+  TRUE: "TRUE",
+  FALSE: "FALSE",
+};
+
+export const parseBooleanValue = (value: string): boolean => {
+  switch (value.toUpperCase()) {
+    case BOOLEAN_VALUE.TRUE:
+      return true;
+    case BOOLEAN_VALUE.FALSE:
+      return false;
+    default:
+      throw new Error("Invalid boolean value");
+  }
+};
