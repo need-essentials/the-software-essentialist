95fe7d9e9909c5c1ba4dab9727eb81b286c9341e
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-22 16:21:02 +0200

    🔵: Remove exports for individual validator functions to keep them private

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,82 +1,19 @@
-import {
-  hasValidTimeFormat,
-  hasValidTimeRangeFormat,
-  isHourInRange,
-  isInRange,
-  isMilitaryTime,
-  isMinuteInRange,
-  isValidTime,
-} from "./index";
+import { isMilitaryTime } from "./index";
 
 describe("military time validator", () => {
-  describe("hasValidTimeFormat", () => {
-    it("should return true if time has valid format", () => {
-      expect(hasValidTimeFormat("12:00")).toBe(true);
-    });
-
-    it("should return false if time has invalid format", () => {
-      expect(hasValidTimeFormat("12:0")).toBe(false);
-    });
+  it("should return true if time range is valid", () => {
+    expect(isMilitaryTime("01:12 - 14:32")).toBeTruthy();
+    expect(isMilitaryTime("22:00 - 23:12")).toBeTruthy();
   });
 
-  describe("hasValidTimeRangeFormat", () => {
-    it("should return true if time range has valid format", () => {
-      expect(hasValidTimeRangeFormat("12:00 - 12:00")).toBe(true);
-    });
-
-    it("should return false if time range has invalid format", () => {
-      expect(hasValidTimeRangeFormat("12:0 - 12:00")).toBe(false);
-    });
+  it("should return false if time range is invalid", () => {
+    expect(isMilitaryTime("25:00 - 12:23")).toBeFalsy();
+    expect(isMilitaryTime("12:76 - 21:00")).toBeFalsy();
   });
 
-  describe("isInRange", () => {
-    it("should return true if value is in range", () => {
-      expect(isInRange(5, 0, 10)).toBe(true);
-    });
-
-    it("should return false if value is not in range", () => {
-      expect(isInRange(5, 6, 10)).toBe(false);
-    });
-  });
-
-  describe("isHourInRange", () => {
-    it("should return true if hour is in range", () => {
-      expect(isHourInRange(5)).toBe(true);
-    });
-
-    it("should return false if hour is not in range", () => {
-      expect(isHourInRange(25)).toBe(false);
-    });
-  });
-
-  describe("isMinuteInRange", () => {
-    it("should return true if minute is in range", () => {
-      expect(isMinuteInRange(5)).toBe(true);
-    });
-
-    it("should return false if minute is not in range", () => {
-      expect(isMinuteInRange(61)).toBe(false);
-    });
-  });
-
-  describe("isValidTime", () => {
-    it("should return true if time is valid", () => {
-      expect(isValidTime("12:00")).toBe(true);
-    });
-
-    it("should return false if time is invalid", () => {
-      expect(isValidTime("25:00")).toBe(false);
-    });
-  });
-
-  describe("isMilitaryTime", () => {
-    it("should return true if time range is valid", () => {
-      expect(isMilitaryTime("01:12 - 14:32")).toBe(true);
-      expect(isMilitaryTime("22:00 - 23:12")).toBe(true);
-    });
-
-    it("should return false if time range is invalid", () => {
-      expect(isMilitaryTime("25:00 - 12:23")).toBe(false);
-    });
+  it("should return false if time range is not in military format", () => {
+    expect(isMilitaryTime("01:12 PM - 02:12 PM")).toBeFalsy();
+    expect(isMilitaryTime("AA:AA - BB:BB")).toBeFalsy();
+    expect(isMilitaryTime("12:00")).toBeFalsy();
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,27 +2,24 @@
 const timePattern = "([0-9]{2}:[0-9]{2})";
 
 // Validation functions
-export const isInRange = (value: number, min: number, max: number): boolean =>
+const isInRange = (value: number, min: number, max: number): boolean =>
   value >= min && value <= max;
-
-export const isHourInRange = (hour: number): boolean => isInRange(hour, 0, 23);
-
-export const isMinuteInRange = (minute: number): boolean =>
-  isInRange(minute, 0, 59);
+const isHourInRange = (hour: number): boolean => isInRange(hour, 0, 23);
+const isMinuteInRange = (minute: number): boolean => isInRange(minute, 0, 59);
 
 // Time format functions
-export const hasValidTimeFormat = (time: string): boolean => {
+const hasValidTimeFormat = (time: string): boolean => {
   const timeFormat = new RegExp(`^${timePattern}$`);
   return timeFormat.test(time);
 };
 
-export const hasValidTimeRangeFormat = (timeRange: string): boolean => {
+const hasValidTimeRangeFormat = (timeRange: string): boolean => {
   const timeRangeFormat = new RegExp(`^${timePattern} - ${timePattern}$`);
   return timeRangeFormat.test(timeRange);
 };
 
 // Time validation functions
-export const isValidTime = (time: string): boolean => {
+const isValidTime = (time: string): boolean => {
   if (!hasValidTimeFormat(time)) return false;
 
   const [hour, minute] = time.split(":").map(Number);
